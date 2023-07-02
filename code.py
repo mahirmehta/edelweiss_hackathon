69b000b4345f34f8a73c158a466b30b5371114d5
Ghostly842 <74295910+soham842op@users.noreply.github.com>
2023-07-02 22:48:12 +0530

    Added jar ,dataset,socket file

---
+++ code.py
@@ -0,0 +1,35 @@
+import subprocess
+import socket
+
+# Define the Java command
+java_command = 'java -Ddebug=True -Dspeed=2 -classpath feed-play-1.0.jar hackathon.player.Main dataset.csv 9011'
+
+# Start the Java program as a subprocess
+subprocess.Popen(java_command, shell=True)
+
+# Wait for the Java program to start
+# Sleep for a few seconds to allow the Java program to initialize
+import time
+time.sleep(2)
+
+# Define the host and port to connect
+host = 'localhost'
+port = 9011
+
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the Java program
+client_socket.connect((host, port))
+
+# Send any initial commands or data if needed
+# client_socket.sendall("Initial command".encode())
+
+# Receive the output from the Java program
+output = client_socket.recv(1024).decode()
+print('Output from Java program:', output)
+
+# Close the socket connection
+client_socket.close()
+
+
